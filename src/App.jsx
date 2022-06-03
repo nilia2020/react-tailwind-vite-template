95301ad9d1f1537cfc8c9d0ba4b09487111e4aca
jorge niglia <jorge.niglia@gmail.com>
2022-06-03 20:34:25 -0300

    template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-2xl font-bold text-blue-500">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
