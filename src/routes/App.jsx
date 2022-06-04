37be5d59b872f4f5ef55019c1318337feba90437
jorge niglia <jorge.niglia@gmail.com>
2022-06-04 12:49:33 -0300

    context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,15 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import AppContext from "../context/AppContext";
 
 const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
+    <AppContext.Provider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </AppContext.Provider>
   );
 };
 
