08921207817bc8a75bfacffc47d97a588211b86f
jorge niglia <jorge.niglia@gmail.com>
2022-06-04 12:46:29 -0300

    template width router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <h1 className="mt-2 text-2xl font-bold text-center text-blue-600">
+      Template React Vite Router Tailwind
+    </h1>
+  );
+};
+
+export default Home;
